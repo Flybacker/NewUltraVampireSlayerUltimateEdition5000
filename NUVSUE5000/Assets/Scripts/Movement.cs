cd8da484380bcd806f922d3a54cc24198e21c4d9
Flybacker <flybacker10@gmail.com>
2025-12-11 09:48:59 +0100

    e

--- NUVSUE5000/Assets/Scripts/Movement.cs
+++ NUVSUE5000/Assets/Scripts/Movement.cs
@@ -7,26 +7,51 @@ public class Movement : MonoBehaviour
     [SerializeField] Rigidbody2D playerRB;
     [SerializeField] float playerSpeed;
     [SerializeField] float jumpPower;
-    [Header("InputLinks")]
-    [SerializeField] InputAction movementAction;
-    [SerializeField] InputAction jumpAction;
-    [SerializeField] InputAction fireAction;
+    [SerializeField] Collider2D groundCheck;
+
+
+    InputAction movementAction;
+    InputAction jumpAction;
+    InputAction fireAction;
+
+    bool canDash;
     bool grounded;
     void Start()
     {
-        
+        movementAction = InputSystem.actions.FindAction("Move");
+        jumpAction = InputSystem.actions.FindAction("Jump");
+        fireAction = InputSystem.actions.FindAction("Attack");
     }
 
     void Update()
     {
+        checkGrounded();
         checkMovement();
+        Debug.Log(grounded);
     }
 
     void checkMovement()
     {
-        if (jumpAction.WasPressedThisFrame() == true && grounded)
+        if ((jumpAction.IsPressed() == true) && grounded)
         {
-
+            playerRB.linearVelocityY = jumpPower;
         }
+        else if ((jumpAction.WasPerformedThisFrame() == true) && canDash)
+        {
+            canDash = false;
+            dash();
+        }
+        playerRB.linearVelocityX = movementAction.ReadValue<Vector2>().x * playerSpeed;
+    }
+
+    void dash()
+    {
+
+    }
+
+
+    void checkGrounded()
+    {
+        grounded = groundCheck.IsTouchingLayers(1 << 3);
     }
 }
